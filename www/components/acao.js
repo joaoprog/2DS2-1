9b4db47d841b3eebf6e483b4bd04f4555f4be2a8
joaoprog <joao2003silvacosta@gmail.com>
2019-07-26 03:10:40 +0900

    Efetuando a soma do App

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,2 +1,7 @@
 // This is a JavaScript file
-
+$(document).on("click","#btnsomar",function(){
+  var valor1 = $("#valor1").val();
+  var valor2 = $("#valor2").val();
+  var result = parseInt(valor1) + parseInt(valor2);
+  $("#resultado").val(result);
+});
